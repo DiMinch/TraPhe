161697e9d2b3aec0bcf2e15770bfc040788c229d
DiMinch <phanduyminh1262@gmail.com>
2026-06-15 10:37:23 +0700

    perf(cache): enable caching and configure non-proxy cache beans

--- traphe-backend/src/main/java/com/example/traphe_backend/TrapheBackendApplication.java
+++ traphe-backend/src/main/java/com/example/traphe_backend/TrapheBackendApplication.java
@@ -2,10 +2,12 @@ package com.example.traphe_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableCaching
 public class TrapheBackendApplication {
 
 	public static void main(String[] args) {
--- traphe-backend/src/main/java/com/example/traphe_backend/config/CacheConfig.java
+++ traphe-backend/src/main/java/com/example/traphe_backend/config/CacheConfig.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,8 +21,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
-@Configuration
-@EnableCaching
+@Configuration(proxyBeanMethods = false)
 public class CacheConfig {
 
     @Bean
